52d18dee6fb927b768813d2972ec94b10b8603ee
Sejal Ahuja <sejalahuja@Sejals-MacBook-Air.local>
2023-01-27 10:20:34 -0600

    initial commit

---
+++ python_review.py
@@ -0,0 +1,20 @@
+
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n") 
+print("hello world")
+
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter another number: "))
+
+total = number_1 + number_2
+
+print(total)
+
+if total > 10: 
+	print("Booo!")
+elif total <= 9:
+	print("That's a small number!")
+else: 
+	print("Your number must be 10!")
+
+for i in range(2):
+	print(i)
